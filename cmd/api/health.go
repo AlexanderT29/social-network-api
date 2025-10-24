932efc40ddca149138523afcf0f94ec4aaff7e03
Alexander <alextesoro96@gmail.com>
2025-10-24 11:13:55 +0200

    aggiunte funzioni per db e main

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,7 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/AlexanderT29/social-network-api/internal/store"
+)
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
+
+	app.store.Posts.Create(r.Context(), &store.Post{})
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,43 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/AlexanderT29/social-network-api/internal/db"
+	"github.com/AlexanderT29/social-network-api/internal/env"
+	"github.com/AlexanderT29/social-network-api/internal/store"
+)
 
 func main() {
 	cfg := config{
 		addr: ":8080",
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social-network-api?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool estrablished")
+
+	store := store.NewStorage(db)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
+
 	mux := app.mount()
 
 	log.Fatal(app.run(mux))
